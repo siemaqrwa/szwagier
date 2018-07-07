5da93e6809f1c590fbdb52e0faed701a300d2e7a
siemaqrwa <siemaqrwa@users.noreply.github.com>
2018-07-07 15:56:12 +0200

    Patch 0.2
    Added:
     -slideshow at homepage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
-
 function Menu(x) {
     var menu = document.getElementById("menu");
-    
+
     x.classList.toggle("active");
     menu.classList.toggle("active");
+}
+
+var actualSlide = 0;
+var translateX = document.getElementById("slideshow");
+var allSlides = document.getElementsByClassName("slide");
+var slideDots = document.getElementsByClassName("dot");
+var slideDuration = setTimeout(slideshow, 5000);
+
+function slideshow() {
+
+    translateX.style.transform = "translateX(-" + (actualSlide * 25) + "%)";
+    
+    for (var i=0; i < slideDots.length; i++) {
+        slideDots[i].className = "dot";
+    }
+    
+    slideDots[actualSlide].classList.add("active");
+
+    actualSlide++;
+
+    if (actualSlide == allSlides.length) {
+        actualSlide = 0;
+    }
+    clearTimeout(slideDuration);
+    slideDuration = setTimeout(slideshow, 5000);
+}
+
+function showSlide(n) {
+    actualSlide = n;
+    slideshow();
 }
