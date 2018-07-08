a00e118e92c675935d2b5e1de03f6437411482ed
siemaqrwa <siemaqrwa@users.noreply.github.com>
2018-07-08 15:34:22 +0200

    Patch 0.3.1
    Added:
      -basic RWD stuff

--- js/main.js
+++ js/main.js
@@ -5,13 +5,23 @@ function Menu(x) {
     menu.classList.toggle("active");
 }
 
-var actualSlide = 0;
+var actualSlide = -1;
 var translateX = document.getElementById("slideshow");
 var allSlides = document.getElementsByClassName("slide");
 var slideDots = document.getElementsByClassName("dot");
 var slideDuration = setTimeout(slideshow, 5000);
 
 function slideshow() {
+
+    actualSlide++;
+
+    if (actualSlide == allSlides.length) {
+        actualSlide = 0;
+    }
+    
+    if (actualSlide <= -1) {
+        actualSlide = allSlides.length - 1;
+    }
 
     translateX.style.transform = "translateX(-" + (actualSlide * 25) + "%)";
     
@@ -20,17 +30,18 @@ function slideshow() {
     }
     
     slideDots[actualSlide].classList.add("active");
-
-    actualSlide++;
-
-    if (actualSlide == allSlides.length) {
-        actualSlide = 0;
-    }
+    
     clearTimeout(slideDuration);
     slideDuration = setTimeout(slideshow, 5000);
 }
 
 function showSlide(n) {
-    actualSlide = n;
+    actualSlide = n - 1;
+    slideshow();
+}
+
+function moveSlide(n) {
+    actualSlide = actualSlide - 1 + n;
+
     slideshow();
 }
