1d10ebe40626af04b43ed094276d784b1f7c89aa
siemaqrwa <siemaqrwa@users.noreply.github.com>
2018-07-09 18:45:58 +0200

    Patch 0.4
    Added:
      -About page
      -Hyperlink to About page
      -RWD stuff for About page
    
    Adjust:
      -CSS splited into 2 files
      -JS splited into 2 files

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 window.addEventListener('load', function () {
+    var blackCover = document.getElementsByClassName("black-cover");
 
     document.getElementById("preloader").classList.add("fuccoff");
-
-    slideshow();
+    
+    
+    for (var i; i < blackCover.length; i++) {
+        blackCover[i].classList.add("fuccoff");
+    }
 
 });
 
 function dontFuccOff() {
     document.getElementById("preloader").classList.remove("fuccoff");
-
-    setTimeout(function () {
-        document.getElementById("preloader").classList.add("fuccoff");
-    }, 5000);
-
+    document.getElementById("preloader").innerHTML = "";
 }
 
 function Menu(x) {
@@ -20,45 +20,4 @@ function Menu(x) {
 
     x.classList.toggle("active");
     menu.classList.toggle("active");
-}
-
-var actualSlide = -1;
-var translateX = document.getElementById("slideshow");
-var allSlides = document.getElementsByClassName("slide");
-var slideDots = document.getElementsByClassName("dot");
-var slideDuration = setTimeout(slideshow, 5000);
-
-function slideshow() {
-
-    actualSlide++;
-
-    if (actualSlide == allSlides.length) {
-        actualSlide = 0;
-    }
-
-    if (actualSlide <= -1) {
-        actualSlide = allSlides.length - 1;
-    }
-
-    translateX.style.transform = "translateX(-" + (actualSlide * 25) + "%)";
-
-    for (var i = 0; i < slideDots.length; i++) {
-        slideDots[i].className = "dot";
-    }
-
-    slideDots[actualSlide].classList.add("active");
-
-    clearTimeout(slideDuration);
-    slideDuration = setTimeout(slideshow, 5000);
-}
-
-function showSlide(n) {
-    actualSlide = n - 1;
-    slideshow();
-}
-
-function moveSlide(n) {
-    actualSlide = actualSlide - 1 + n;
-
-    slideshow();
 }
---
+++ js/slideshow.js
@@ -0,0 +1,46 @@
+var actualSlide = -1;
+var translateX = document.getElementById("slideshow");
+var allSlides = document.getElementsByClassName("slide");
+var slideDots = document.getElementsByClassName("dot");
+var slideDuration = setTimeout(slideshow, 5000);
+
+window.addEventListener('load', function () {
+
+    slideshow();
+
+});
+
+function slideshow() {
+
+    actualSlide++;
+
+    if (actualSlide == allSlides.length) {
+        actualSlide = 0;
+    }
+
+    if (actualSlide <= -1) {
+        actualSlide = allSlides.length - 1;
+    }
+
+    translateX.style.transform = "translateX(-" + (actualSlide * 25) + "%)";
+
+    for (var i = 0; i < slideDots.length; i++) {
+        slideDots[i].className = "dot";
+    }
+
+    slideDots[actualSlide].classList.add("active");
+
+    clearTimeout(slideDuration);
+    slideDuration = setTimeout(slideshow, 5000);
+}
+
+function showSlide(n) {
+    actualSlide = n - 1;
+    slideshow();
+}
+
+function moveSlide(n) {
+    actualSlide = actualSlide - 1 + n;
+
+    slideshow();
+}
