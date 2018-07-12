c3c44c89c51f0efa55a316762e1318586085eafe
siemaqrwa <siemaqrwa@users.noreply.github.com>
2018-07-12 14:21:42 +0200

    Patch 0.6
    Added:
      -easeScroll
      -basic text on silides

--- js/main.js
+++ js/main.js
@@ -21,3 +21,16 @@ function Menu() {
     menuIcon.classList.toggle("active");
     menu.classList.toggle("active");
 }
+
+$("html").easeScroll({
+  frameRate: 60,
+  animationTime: 1000,
+  stepSize: 120,
+  pulseAlgorithm: !0,
+  pulseScale: 8,
+  pulseNormalize: 1,
+  accelerationDelta: 20,
+  accelerationMax: 1,
+  keyboardSupport: !0,
+  arrowScroll: 50
+});
