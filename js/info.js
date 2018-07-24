83d52aaefaa8b80053d9e6b6ce6aea7dacb3ab3f
siemaqrwa <siemaqrwa@users.noreply.github.com>
2018-07-24 18:38:57 +0200

    Patch 0.10.3
    Added:
      -Smaller version of easeScroll
    
    Adjust:
      -removing unused stuff

--- js/info.js
+++ js/info.js
@@ -1,9 +1,9 @@
 function infoBox() {
     var infoIcon = document.getElementById("info-icon");
     var infoBox = document.getElementById("top-right");
-    
+
     infoBox.classList.toggle("active");
-    
+
     infoIcon.classList.toggle("fa-arrow-right");
     infoIcon.classList.toggle("fa-info");
 }
