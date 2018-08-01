a5c3bdfd7226f9658c18c77b6decdb6b41ddaf1f
siemaqrwa <siemaqrwa@users.noreply.github.com>
2018-08-01 15:34:02 +0200

    Patch 0.13.3
    Adjust:
      -smaller files

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
 
     document.getElementById("preloader").classList.add("fuccoff");
-
+    
     document.getElementById("grid2").classList.add("fuccoff");
 
 });
@@ -39,4 +39,14 @@ if (isIEorEDGE() == false) {
         $("body").easeScroll();
     });
 
+}
+
+function infoBox() {
+    var infoIcon = document.getElementById("info-icon");
+    var infoBox = document.getElementById("top-right");
+
+    infoBox.classList.toggle("active");
+
+    infoIcon.classList.toggle("fa-arrow-right");
+    infoIcon.classList.toggle("fa-info");
 }
